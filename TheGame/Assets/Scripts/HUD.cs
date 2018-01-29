af61751a2e91ab7995924ddd9482cff7c8528936
mattyp13 <mpokoik@live.com>
2018-01-29 12:37:54 -0600

    Health and Pause Menu
    Added a Heart health bar and spikes to run into. If you hit the spikes
    you take damage. Made pause menu. Main menu button won't do anything
    since we don't have a main menu yet (which is why i made it red when you
    hover). Right now to pause you hit escape, i will have to add in an on
    screen button for it next since phones don't have an escape key. Also if
    you lose (hitting then spike when you have zero hearts) it will restart
    the game.

---
+++ TheGame/Assets/Scripts/HUD.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HUD : MonoBehaviour {
+
+    public Sprite[] HeartSprites;
+    public Image HeartUI;
+    private PlayerScript Player;
+
+    // Use this for initialization
+    void Start () {
+        Player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerScript>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //matches curHealth to Heartsprites array number for the correct heart pic 
+        HeartUI.sprite = HeartSprites[Player.curHealth];
+    }
+}
--- TheGame/Assets/Scripts/PlayerScript.cs
+++ TheGame/Assets/Scripts/PlayerScript.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerScript : MonoBehaviour {
 
     private int playerSpeed = 5;
     private int direction = 1;
-	
-        // Use this for initialization
-	void Start () {
-		
+
+    public int curHealth = 0;
+    public int maxHealth = 2;
+
+    
+
+    // Use this for initialization
+    void Start () {
+     
 	}
 	
 	// Update is called once per frame
@@ -29,6 +35,25 @@ public class PlayerScript : MonoBehaviour {
         else if (moveX < 0)
         {
             direction = -1;
+        }
+    }
+
+    //takes away health point  
+    public void Damage(int dmg)
+    {
+        //checks to see if hit will kill you 
+        if (curHealth + dmg > maxHealth)
+        {  
+            //resets back to starting health
+            curHealth = 0;
+            print("GAME OVER!");
+            //restarts the level. Can change later to send to main menu
+            Application.LoadLevel(Application.loadedLevel);
+        }
+        //changes adds damage to current health
+        else
+        {
+            curHealth += dmg;
         }
     }
 }
