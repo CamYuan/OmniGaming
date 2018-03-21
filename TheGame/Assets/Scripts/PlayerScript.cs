80c535654f13cd0bbb21d81b1048f623ca9d9799
jfsvzb <jfsvzb@mail.missouri.edu>
2018-03-20 19:02:21 -0500

    Jump controls added

--- TheGame/Assets/Scripts/PlayerScript.cs
+++ TheGame/Assets/Scripts/PlayerScript.cs
@@ -7,11 +7,21 @@ public class PlayerScript : MonoBehaviour {
 
     private int playerSpeed = 5;
     private int direction = 1;
+    private float vectorY = 0.0f;
+
+    public float jumpSpeed = 10.0f;
+    public float forceY = 0;
+    private float invertGrav;
+    public float gravity = 20.0F;
+    public float gravityForce = 3.0f;
+    public float airTime = 2f;
 
     public int curHealth = 0;
     public int maxHealth = 2;
 
     private DeathMenu Death;
+    private BoxCollider2D controller;
+    public BoxCollider2D ground;
 
 
     //knockback testing
@@ -21,8 +31,8 @@ public class PlayerScript : MonoBehaviour {
     // Use this for initialization
     void Start () {
         Death = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<DeathMenu>();
-
-
+        invertGrav = gravity * airTime;
+        controller = GetComponent<BoxCollider2D>();
         //knockback testing
         //rb2D = GetComponent<Rigidbody2D>();
     }
@@ -31,25 +41,47 @@ public class PlayerScript : MonoBehaviour {
 	void Update () {
         Touch_Move_Player();
         Keyboard_Move_Player();
-        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(direction * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
-
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(direction * playerSpeed, vectorY);
     }
 
     void Touch_Move_Player()
     {
-        if (Input.touchCount > 0)
+        if (controller.IsTouching(ground))
         {
-            var touch = Input.GetTouch(0);
-            if (touch.position.x < Screen.width / 2)
+            forceY = 0;
+            invertGrav = gravity * airTime;
+            if (Input.touchCount > 0 || Input.GetKey(KeyCode.Space))
             {
-                direction = -1;
+                forceY = jumpSpeed;
+            }
+        }
+        else
+        {
+            if ((Input.touchCount > 0 || Input.GetKey(KeyCode.Space)))
+            {
+                forceY -= gravity * Time.deltaTime;
             }
             else
             {
-                direction = 1;
+                forceY -= gravity * Time.deltaTime * 2;
             }
         }
+        vectorY = forceY;
     }
+
+    //keeping for future use maybe, not needed for now
+    /*void Touch_Move_Player()
+    {
+        var touch = Input.GetTouch(0);
+        if (touch.position.x < Screen.width / 2)
+        {
+            direction = -1;
+        }
+        else
+        {
+            direction = 1;
+        }
+    }*/
 
     //Keyboard controls
     void Keyboard_Move_Player()
